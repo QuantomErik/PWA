ae9501e731879c9a6d176cd77f4b726ebbb98f64
QuantomErik <ey222ci@student.lnu.se>
2023-12-29 02:04:34 +0100

    Work on weatherApp, add geolocation, add forecast API, fix GUI

--- src/js/components/customApp/customApp.js
+++ src/js/components/customApp/customApp.js
@@ -5,24 +5,46 @@ const template = document.createElement('template')
 template.innerHTML = `
   <style>
 
+#Window::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    backdrop-filter: blur(10px);
+    z-index: -1;
+}
+
 #Window {
         position: relative; 
         display: flex;
     flex-direction: column;
-    border: 1px solid #ddd;
+    /* border: 1px solid #ddd; */
+    
     border-radius: 8px;
     box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-    max-width: 400px;
-    max-height: 600px;
+    /* max-width: 400px;
+    max-height: 600px; */
     overflow: hidden;
-    width: 380px;
-    height: 440px;
+    width: 390px;
+    height: 600px;
+    /* transition: width 0.5s ease, height 1s ease; */
+    border: 1px solid rgba(255, 255, 255, 0.5);
+    /* background: radial-gradient(circle, rgba(201, 77, 212, 0.7), rgba(75, 19, 79, 0.7)); */
 
     
 
     margin: 20px;
     /* background-color: #f9f9f9; */
-    background: linear-gradient(to right, #b0bbe7 0%, #1f2e5c 100%);
+    /* background: linear-gradient(to right, #b0bbe7 0%, #1f2e5c 100%); */
+    /* background: radial-gradient(circle, #b0bbe7, #1f2e5c); */
+    /* background: radial-gradient(circle, #c94dd4, #4b134f); */
+    /* background: linear-gradient(to right, #c94dd4 0%, #4b134f 100%); */
+    background: radial-gradient(circle, rgba(201, 77, 212, 0.7), rgba(75, 19, 79, 0.7));
+
+    
+    
     text-align: center;
     align-items: center; 
     max-width: none;
@@ -31,15 +53,21 @@ template.innerHTML = `
     
 }
 
+/* #Window.expanded {
+    width: 390px;  
+    height: 600px; 
+} */
+
 #dragHandle {
     margin-bottom: 5px;
-    background-color: orange;
+    /* background-color: orange; */
     color: transparent;
     width: 100%;
     height: 30px;
     align-items: center; 
     display: flex; 
     justify-content: center;
+    /* background: radial-gradient(circle, rgba(201, 77, 212, 0.7), rgba(75, 19, 79, 0.7)); */
     
   }
 
@@ -59,6 +87,7 @@ template.innerHTML = `
         position: relative; 
     display: flex;
     align-items: center; 
+    
 
     }
 
@@ -66,14 +95,20 @@ template.innerHTML = `
         flex-grow: 0;
         border: none;
     padding: 0px;
-    background: linear-gradient(to right, #5a5b5b 0%, #2a2b2b 100%);
-    color: #23b82a;
+    /* background: linear-gradient(to right, #5a5b5b 0%, #2a2b2b 100%); */
+    /* color: #23b82a; */
+    color: white;
     font-weight: bold;
     resize: none;
     height: 35px;
     width: 300px;
     border-radius: 8px;
     padding-left: 40px;
+    background: transparent;
+    /* box-shadow: 0 0 5px rgba(0, 0, 0, 0.2); */
+    /* border: 1px solid #DDD; */
+    /* border: 1px solid rgba(255, 255, 255, 0.5); */
+    box-shadow: 0 0 8px rgba(255, 255, 255, 0.5);
 
     
     }
@@ -106,13 +141,15 @@ template.innerHTML = `
     outline: none;
 }
 
-#positionIcon {
+#positionButton {
     position: absolute;
     left: 5px; 
     width: 30px; 
     height: 30px; 
     background: url("${IMG_URL2}") no-repeat center/50%;
     background-size: 60%;
+    border: none; 
+    cursor: pointer; 
     
     
 }
@@ -125,51 +162,156 @@ template.innerHTML = `
 #weatherImage {
     height: 200px;
     width: 200px;
+    visibility: hidden;
 }
 
 #windSpeedContainer, #humidityContainer {
     display: flex;
     align-items: center;
+    font-weight: bold;
+    /* color: #58D8F8; */
+    color: white;
 }
 
 #windSpeedContainer {
     position: absolute;
     left: 0;
     bottom: 0;
-    margin-left: 20px;
+    margin-left: 25px;
     text-align: left;
-    margin-bottom: 10px;
+    margin-bottom: 20px;
+    visibility: hidden;
 }
 
 #humidityContainer {
     position: absolute;
     right: 0;
     bottom: 0;
-    margin-right: 20px;
+    margin-right: 25px;
     text-align: left;
-    margin-bottom: 10px;
+    margin-bottom: 20px;
+    visibility: hidden;
 }
 
 #windIcon{
-    width: 30px; 
-    height: 30px; 
+    width: 40px; 
+    height: 40px; 
     margin-right: 5px; 
+    
 }
 
 #humidityIcon {
-    width: 30px; 
-    height: 30px; 
+    width: 40px; 
+    height: 40px; 
     margin-right: 5px; 
+    
 }
 
 #temperature {
-    font-size: 30px;
+    font-size: 40px;
     font-weight: bold;
+    color: white;
 }
 
 #weatherState {
-
+  color: white;
+  font-size: 20px;
 }
+
+#cityNameDisplay {
+    color: white;
+    margin-top: 5px; 
+    font-size: 20px; 
+}
+
+/* .forecast-row {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin-top: 20px;
+}
+
+.forecast-card {
+    border: 1px solid #ddd;
+    padding: 10px;
+    margin: 5px;
+    text-align: center;
+    width: 18%; 
+    height: 100px;
+}
+
+.forecast-icon {
+    width: 50px;
+    height: 50px;
+    margin: 5px 0;
+}
+
+.forecast-date, .forecast-temp, .forecast-desc {
+    margin: 5px 0;
+}
+
+#forecastContainer {
+  display: flex;
+    justify-content: space-around; 
+    align-items: center;
+    flex-wrap: nowrap;
+} */
+
+#hourlyContainer {
+    display: flex;
+    overflow-x: auto;
+    white-space: nowrap;
+    /* padding: 10px; */
+    /* background-color: blue; */
+    width: 340px;
+    margin-top: 10px;
+    justify-content: center;
+}
+
+.weatherSlot {
+    flex: none;
+    margin-right: 5px;
+    /* padding: 5px; */
+    /* border: 1px solid #ccc; */
+    /* background-color: blue; */
+    border-radius: 5px;
+    width: 50px;
+    height: 120px;
+    font-size: 15px;
+}
+
+#hourlyContainer::-webkit-scrollbar {
+    display: none;
+}
+
+/* Make the scrollbar visible when hovering over the container */
+#hourlyContainer:hover::-webkit-scrollbar {
+    display: block;
+}
+
+/* Style the scrollbar track */
+#hourlyContainer::-webkit-scrollbar-track {
+    background-color: #f0f0f0;
+    border-radius: 10px;
+}
+
+/* Style the scrollbar handle */
+#hourlyContainer::-webkit-scrollbar-thumb {
+    background-color: #888;
+    border-radius: 10px;
+}
+
+#hourlyContainer::-webkit-scrollbar-thumb:hover {
+    background: #555;
+}
+
+/* Style the scrollbar itself */
+#hourlyContainer::-webkit-scrollbar {
+    width: 8px;
+    height: 8px;
+}
+
+
 
 
 </style>
@@ -180,12 +322,12 @@ template.innerHTML = `
   </div>
   
    <div id="searchInputContainer">
-    <span id="positionIcon"></span>
+    <button id="positionButton"></button>
     <input type="text" id="searchBox" placeholder="Search City..." />
-   <button id="searchButton">
-    
-   </button>
+   <button id="searchButton"></button>
    </div>
+
+   <div id="cityNameDisplay"></div>
    
    <div id="weatherImageContainer">
     <img id="weatherImage" src="" alt="Weather Image">
@@ -194,6 +336,25 @@ template.innerHTML = `
    <div id="weatherInfo">
     <div id="temperature"></div>
     <div id="weatherState"></div>
+
+    <div id="hourlyContainer"></div>
+
+    <!-- <div id="forecastContainer">
+    <div class="forecast-card">
+        <div class="forecast-date">Date 1</div>
+        <img src="path/to/weather-icon1.png" alt="Weather Icon" class="forecast-icon">
+        <div class="forecast-temp">Temp 1°C</div>
+        <div class="forecast-desc">Description 1</div>
+    </div>
+
+    <div class="forecast-card">
+        <div class="forecast-date">Date 2</div>
+        <img src="path/to/weather-icon2.png" alt="Weather Icon" class="forecast-icon">
+        <div class="forecast-temp">Temp 2°C</div>
+        <div class="forecast-desc">Description 2</div>
+    </div>
+
+    </div> -->
 
 
     <div id="windSpeedContainer">
@@ -205,7 +366,7 @@ template.innerHTML = `
     </div>
 
     <div id="humidityContainer">
-        <img id="humidityIcon" src="js/components/customApp/images/humidity.png" alt="Humidity Icon">
+        <img id="humidityIcon" src="js/components/customApp/images/droplet.png" alt="Humidity Icon">
         <div>
         <div id="humidityValue"></div>
             <div>Humidity</div>
@@ -230,6 +391,16 @@ customElements.define('custom-app',
    *
    */
   class extends HTMLElement {
+
+    weatherStateImages = {
+      snow: 'js/components/customApp/images/snowing.png',
+      clouds: 'js/components/customApp/images/cloud.png',
+      mist: 'js/components/customApp/images/rain-drops.png',
+      haze: 'js/components/customApp/images/cloudy-day.png',
+      clear: 'js/components/customApp/images/sun.png',
+      rain: 'js/components/customApp/images/raining.png',
+      sunny: 'js/components/customApp/images/sun.png'
+    }
     /**
      *
      */
@@ -239,6 +410,8 @@ customElements.define('custom-app',
       this.shadowRoot.appendChild(template.content.cloneNode(true))
 
       this.searchButton = this.shadowRoot.querySelector('#searchButton')
+      this.searchBox = this.shadowRoot.querySelector('#searchBox')
+      this.positionButton = this.shadowRoot.querySelector('#positionButton')
     }
 
     /**
@@ -293,6 +466,13 @@ customElements.define('custom-app',
       dragHandle.addEventListener('mousedown', (event) => this.handleDragStart(event))
 
       this.searchButton.addEventListener('click', () => this.fetchWeather())
+      this.positionButton.addEventListener('click', () => this.getLocation())
+
+      this.searchBox.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') {
+          this.fetchWeather()
+        }
+      })
 
       /* this.findMcDonaldsButton.addEventListener('click', () => this.findLocations()) */
       /* const mcDonaldsButton = this.shadowRoot.getElementById('mcDonalds')
@@ -308,14 +488,18 @@ customElements.define('custom-app',
      *
      */
     async fetchWeather () {
+      
+      this.shadowRoot.getElementById('weatherImage').style.visibility = 'visible'
       console.log('fetching weather')
 
       const APIkey = '6dc4f57a3bc1d883f18bc90fda0a6973'
       const searchBox = this.shadowRoot.getElementById('searchBox')
       const city = searchBox.value
+      const cityNameDisplay = this.shadowRoot.getElementById('cityNameDisplay')
 
       if (!city) {
         console.error('No city provided.')
+        cityNameDisplay.textContent = ''
         return
       }
 
@@ -328,11 +512,22 @@ customElements.define('custom-app',
         }
         const data = await response.json()
 
-        this.shadowRoot.getElementById('temperature').textContent = `${data.main.temp}°C`
+        const roundedTemp = Math.round(data.main.temp)
+
+        this.shadowRoot.getElementById('temperature').textContent = `${roundedTemp}°C`
         this.shadowRoot.getElementById('windSpeedValue').textContent = `${data.wind.speed} m/s`
         this.shadowRoot.getElementById('humidityValue').textContent = `${data.main.humidity}%`
         this.shadowRoot.getElementById('weatherState').textContent = `${data.weather[0].main}`
+
+        this.shadowRoot.getElementById('windSpeedContainer').style.visibility = 'visible'
+        this.shadowRoot.getElementById('humidityContainer').style.visibility = 'visible'
         /* (${data.weather[0].description}) */
+        searchBox.value = ''
+
+        cityNameDisplay.textContent = `${city}`
+        cityNameDisplay.textContent = this.capitalizeFirstLetter(city)
+        this.shadowRoot.getElementById('Window').classList.add('expanded')
+        await this.fetchWeatherForecast(city)
 
         const weatherStateImages = {
           snow: 'js/components/customApp/images/snowing.png',
@@ -352,10 +547,161 @@ customElements.define('custom-app',
         } else {
           console.error('No image found for this weather state:', weatherState)
         }
+        
 
         console.log(data)
       } catch (error) {
         console.error('Error fetching weather data:', error)
       }
     }
+
+    capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+    }
+
+
+    async fetchWeatherForecast(city) {
+      console.log('fetching weather forecast')
+  
+      const APIkey = '6dc4f57a3bc1d883f18bc90fda0a6973';
+      const url = `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(city)}&units=metric&appid=${APIkey}`
+  
+      try {
+          const response = await fetch(url);
+          if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`)
+          }
+          const forecastData = await response.json()
+  
+          
+          this.displayHourlyForecast(forecastData)
+      } catch (error) {
+          console.error('Error fetching weather forecast:', error)
+      }
+  }
+
+  displayHourlyForecast(forecastData) {
+    const hourlyContainer = this.shadowRoot.getElementById('hourlyContainer')
+    hourlyContainer.innerHTML = ''
+
+    const weatherStateImages = {
+      snow: 'js/components/customApp/images/snowing.png',
+      clouds: 'js/components/customApp/images/cloud.png',
+      mist: 'js/components/customApp/images/rain-drops.png',
+      haze: 'js/components/customApp/images/cloudy-day.png',
+      clear: 'js/components/customApp/images/sun.png',
+      rain: 'js/components/customApp/images/raining.png',
+      sunny: 'js/components/customApp/images/sun.png'
+  }
+
+    const currentTime = new Date()
+    const next24Hours = forecastData.list.filter(item => {
+        const itemTime = new Date(item.dt * 1000)
+        return itemTime > currentTime && itemTime < new Date(currentTime.getTime() + 24 * 60 * 60 * 1000)
+    });
+
+    next24Hours.forEach(item => {
+
+      const itemTime = new Date(item.dt * 1000)
+      const hours = itemTime.getHours() 
+      const formattedHour = hours < 10 ? `0${hours}` : `${hours}`
+
+      const roundedTemp = Math.round(item.main.temp)
+
+      const weatherState = item.weather[0].main.toLowerCase()
+        const weatherIconSrc = weatherStateImages[weatherState] || 'default-icon-path'
+
+        const weatherDiv = document.createElement('div')
+        weatherDiv.className = 'weatherSlot'
+        weatherDiv.innerHTML = `
+            <p>${formattedHour}</p>
+            <img src="${weatherIconSrc}" alt="${item.weather[0].main}"  style="width: 30px; height: 30px;">
+            <p>${roundedTemp}°C</p>`
+        hourlyContainer.appendChild(weatherDiv)
+    })
+}
+
+async getLocation() {
+  if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+          console.log("Latitude: " + position.coords.latitude)
+          console.log("Longitude: " + position.coords.longitude)
+
+          fetchWeatherByCoordinates(position.coords.latitude, position.coords.longitude)
+      }, function(error) {
+          console.error("Error Code = " + error.code + " - " + error.message)
+      })
+  } else {
+      console.error("Geolocation is not supported by this browser.")
+  }
+}
+
+// Call this function when your app initializes
+/* this.getLocation(); */
+
+
+  
+    
+    /* async fetchWeatherForecast (city) {
+      console.log('fetching weatherForcast')
+
+      const APIkey = '6dc4f57a3bc1d883f18bc90fda0a6973'
+
+      
+
+      if (!city) {
+        console.error('No city provided for forecast.')
+        return
+      }
+
+      const url = `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(city)}&appid=${APIkey}`
+
+      try {
+        const response = await fetch(url)
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        const forecastData = await response.json()
+
+
+        
+        console.log(forecastData) 
+      } catch (error) {
+        console.error('Error fetching weather forecast:', error)
+      }
+    } */
+
+    /**
+     *
+     * @param forecastData
+     */
+    /* async processForecastData (forecastData) {
+
+      console.log('Processing forecast data:', forecastData)
+      
+      const dailyForecasts = forecastData.list.filter(forecast =>
+        new Date(forecast.dt * 1000).getHours() === 12
+      ).slice(0, 5)
+
+      let forecastHtml = '<div class="forecast-row">'
+      dailyForecasts.forEach(forecast => {
+        const date = new Date(forecast.dt * 1000).toLocaleDateString()
+        const temp = forecast.main.temp
+        const weatherMain = forecast.weather[0].main.toLowerCase()
+        const weatherIconSrc = this.weatherStateImages[weatherMain] || 'default-icon-path'
+
+        forecastHtml += `
+              <div class="forecast-card">
+                  <div class="forecast-date">${date}</div>
+                  <img src="${weatherIconSrc}" alt="${weatherMain}" class="forecast-icon">
+                  <div class="forecast-temp">${temp}°C</div>
+                  <div class="forecast-desc">${forecast.weather[0].description}</div>
+              </div>
+          `
+      })
+      forecastHtml += '</div>'
+
+      const forecastContainer = this.shadowRoot.getElementById('forecastContainer')
+      forecastContainer.append(forecastHtml)
+    } */
   })
