86c76cd3c5d6eedafc61b8eff1a9144b5a27a230
QuantomErik <ey222ci@student.lnu.se>
2023-12-30 23:37:08 +0100

    Add manifest.

--- src/js/index.js
+++ src/js/index.js
@@ -69,7 +69,7 @@ createDock()
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
       try {
-          const registration = await this.navigator.serviceWorker.register('/serviceworker.js')
+          const registration = await navigator.serviceWorker.register('/serviceworker.js')
 
           console.log('ServiceWorker: Registration successful with scope: ', registration.scope)
       } catch (error) {
