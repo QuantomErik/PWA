37f23e782da55186117c1ccec98ec5b2e2959316
QuantomErik <ey222ci@student.lnu.se>
2024-01-03 19:16:26 +0100

    Add JSDOC to dock.js

--- src/js/components/dock/dock.js
+++ src/js/components/dock/dock.js
@@ -1,15 +1,13 @@
-/* import '../messageApp/index.js' */
 const IMG_MEMORY = (new URL('images/memory-game.png', import.meta.url)).href
 const IMG_CHAT = (new URL('images/chat-box.png', import.meta.url)).href
 const IMG_WEATHER = (new URL('images/weather-app.png', import.meta.url)).href
 
 const template = document.createElement('template')
-
 template.innerHTML = `
 <style>
   #app-dock {
     display: flex;
-    flex-direction: column; 
+    flex-direction: column;
     justify-content: start;
     align-items: center;
     position: fixed;
@@ -28,11 +26,9 @@ template.innerHTML = `
   }
 
   .loading-indicator {
-      
-      
-      display: none;
-    border: 5px solid #f3f3f3; 
-    border-top: 5px solid #3498db; 
+    display: none;
+    border: 5px solid #f3f3f3;
+    border-top: 5px solid #3498db;
     border-radius: 50%;
     width: 50px;
     height: 50px;
@@ -45,50 +41,64 @@ template.innerHTML = `
   }
 
 </style>
-
 <div class="loading-indicator"></div>
 <div id="app-dock">
-  <img id="memoryGameIcon" src="${IMG_MEMORY}" class="app-icon">
-  <img id="messagesAppIcon" src="${IMG_CHAT}" class="app-icon">
-  <img id="customAppIcon" src="${IMG_WEATHER}" class="app-icon">
-
-  
+  <img id="memoryGameIcon" src="${IMG_MEMORY}" class="app-icon" tabindex="0" alt="Memory Game">
+  <img id="messagesAppIcon" src="${IMG_CHAT}" class="app-icon" tabindex="0" alt="Messages App">
+  <img id="customAppIcon" src="${IMG_WEATHER}" class="app-icon" tabindex="0" alt="Weather App">
 </div>
 `
 
 window.customElements.define('app-dock',
 
   /**
-   *
+   * Represents a dock for launching different applications such as Memory Game, Messages App, and Weather App.
    */
   class extends HTMLElement {
     /**
+     * Private collection of icon elements in the dock.
      *
+     * @type {Element[]}
+     */
+    #icons
+
+    /**
+     * Private collection of icon elements in the dock.
+     *
+     * @type {number}
+     */
+    #loadingTimeout
+    /**
+     * Private handle for managing the loading timeout.
      */
     constructor () {
       super()
       this.attachShadow({ mode: 'open' })
       this.shadowRoot.appendChild(template.content.cloneNode(true))
+      this.#icons = this.shadowRoot.querySelectorAll('.app-icon')
+      this.#loadingTimeout = null
     }
 
     /**
-     *
+     * Called after the element is inserted into the DOM.
      */
     connectedCallback () {
       this.shadowRoot.getElementById('app-dock').addEventListener('click', (event) => {
-        this.handleIconClick(event)
+        this.#handleIconClick(event)
       })
 
-      /* this.shadowRoot.getElementById('memoryGameIcon').addEventListener('click', () => this.openApp('memoryGame'))
-      this.shadowRoot.getElementById('messagesAppIcon').addEventListener('click', () => this.openApp('messagesApp'))
-      this.shadowRoot.getElementById('customAppIcon').addEventListener('click', () => this.openApp('customApp')) */
+      this.#icons.forEach(icon => {
+        icon.addEventListener('keydown', (event) => this.handleArrowKeyPress(event, this.#icons))
+      })
     }
 
     /**
+     * Handles click events on the app icons.
      *
-     * @param event
+     * @param {Event} event - The event object associated with the click.
+     * @private
      */
-    handleIconClick (event) {
+    #handleIconClick (event) {
       const clickedElement = event.target
 
       // Check if the clicked element or its parent is an app icon
@@ -102,32 +112,16 @@ window.customElements.define('app-dock',
     }
 
     /**
+     * Opens a specified application.
      *
-     * @param appName
-     */
-
-    /**
-     *
-     * @param appName
+     * @param {string} appName - The name of the application to open.
      */
     openApp (appName) {
       console.log(`Opening ${appName}`)
-
       let loadingTimeout
 
-      /**
-       *
-       */
-      const showLoadingIndicatorAfterDelay = () => {
-        loadingTimeout = setTimeout(() => {
-          if (!this.appLoaded) {
-            this.showLoadingIndicator()
-          }
-        }, 100) // Delay in milliseconds
-      }
-
       this.appLoaded = false
-      showLoadingIndicatorAfterDelay()
+      this.#showLoadingIndicatorAfterDelay()
 
       switch (appName) {
         case 'messagesApp':
@@ -135,12 +129,12 @@ window.customElements.define('app-dock',
           import('../messageApp/index.js').then(module => {
             this.appLoaded = true
             clearTimeout(loadingTimeout)
-            this.hideLoadingIndicator()
+            this.#hideLoadingIndicator()
             this.dispatchEvent(new CustomEvent('start-message-app', { bubbles: true }))
           }).catch(err => {
             console.error('Failed to load the Message App', err)
             clearTimeout(loadingTimeout)
-            this.hideLoadingIndicator()
+            this.#hideLoadingIndicator()
           })
           break
 
@@ -148,12 +142,12 @@ window.customElements.define('app-dock',
           import('../memoryGame/index.js').then(module => {
             this.appLoaded = true
             clearTimeout(loadingTimeout)
-            this.hideLoadingIndicator()
+            this.#hideLoadingIndicator()
             this.dispatchEvent(new CustomEvent('start-memory-game', { bubbles: true }))
           }).catch(err => {
             console.error('Failed to load the Memory Game', err)
             clearTimeout(loadingTimeout)
-            this.hideLoadingIndicator()
+            this.#hideLoadingIndicator()
           })
           break
 
@@ -161,12 +155,12 @@ window.customElements.define('app-dock',
           import('../customApp/index.js').then(module => {
             this.appLoaded = true
             clearTimeout(loadingTimeout)
-            this.hideLoadingIndicator()
+            this.#hideLoadingIndicator()
             this.dispatchEvent(new CustomEvent('start-custom-app', { bubbles: true }))
           }).catch(err => {
             console.error('Failed to load the Custom App', err)
             clearTimeout(loadingTimeout)
-            this.hideLoadingIndicator()
+            this.#hideLoadingIndicator()
           })
           break
 
@@ -178,17 +172,63 @@ window.customElements.define('app-dock',
     }
 
     /**
+     * Shows the loading indicator after a brief delay.
      *
+     * @private
      */
-    showLoadingIndicator () {
+    #showLoadingIndicatorAfterDelay () {
+      // Clear any existing timeout
+      clearTimeout(this.#loadingTimeout)
+
+      // Set a new timeout
+      this.#loadingTimeout = setTimeout(() => {
+        if (!this.appLoaded) {
+          this.#showLoadingIndicator()
+        }
+      }, 100) // Delay in milliseconds
+    }
+
+    /**
+     * Displays the loading indicator.
+     *
+     * @private
+     */
+    #showLoadingIndicator () {
       this.shadowRoot.querySelector('.loading-indicator').style.display = 'block'
     }
 
     /**
+     * Hides the loading indicator.
      *
+     * @private
      */
-    hideLoadingIndicator () {
+    #hideLoadingIndicator () {
       this.shadowRoot.querySelector('.loading-indicator').style.display = 'none'
+    }
+
+    /**
+     * Handles keyboard navigation using arrow keys within the dock icons.
+     *
+     * @param {KeyboardEvent} event - The keyboard event object.
+     * @type  {Element[]} icons - A collection of icon elements to navigate through.
+     */
+    handleArrowKeyPress (event, icons) {
+      const currentIndex = Array.from(icons).indexOf(event.target)
+      let newIndex = currentIndex
+      switch (event.key) {
+        case 'ArrowUp':
+        case 'ArrowLeft':
+          // Move focus to the previous icon
+          newIndex = currentIndex > 0 ? currentIndex - 1 : icons.length - 1
+          break
+        case 'ArrowDown':
+        case 'ArrowRight':
+          // Move focus to the next icon
+          newIndex = currentIndex < icons.length - 1 ? currentIndex + 1 : 0
+          break
+      }
+
+      icons[newIndex].focus()
     }
   }
 
