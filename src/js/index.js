064fee1f18e254a24f984ba924ec32a82ab2af17
QuantomErik <ey222ci@student.lnu.se>
2024-01-03 15:53:24 +0100

    test

--- src/js/index.js
+++ src/js/index.js
@@ -66,7 +66,7 @@ createDock()
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', async () => {
     try {
-      const registration = await navigator.serviceWorker.register('/src/serviceworker.js')
+      const registration = await navigator.serviceWorker.register('/serviceworker.js')
 
       console.log('ServiceWorker: Registration successful with scope: ', registration.scope)
     } catch (error) {
